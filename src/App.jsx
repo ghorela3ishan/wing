99653c1436cf015fc792de26ef9534826d414d4c
priyamvada.pant <ghorela.ishan@gmail.com>
2019-03-31 22:56:22 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component} from "react";
+
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Finally it's working</h1>
+            </div>
+        );
+    }
+}
