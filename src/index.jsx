2f4c77ac2076c67b0b2853f91e4bdfadac3d268e
priyamvada.pant <ghorela.ishan@gmail.com>
2019-04-17 22:39:20 +0530

    router configuration

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,23 @@ import { Provider } from "react-redux";
 import store from "./core/Store";
 import rootReducer from "./services/rootReducer";
 import saga from "./services/saga";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 // import { IntlProvider } from "react-intl";
 // import translation from "./core/Internationalization/internationalization";
+import About from "./components/About"; //to be removed later. only for demo purpose
 
 
 const storeMain = store.provideStore(rootReducer, saga);
 
 ReactDOM.render(
     // <IntlProvider locale={translation.getLanguage()} messages={translation.getTransalations()}>
-        <Provider store={storeMain}>
-            <App/>
-        </Provider>
+        <Router>
+            <Switch>
+                <Provider store={storeMain}>
+                    <Route exact path="/" component={App} />
+                    <Route exact path="/about" component={About} /> 
+                </Provider>
+            </Switch>
+        </Router>
     // </IntlProvider>
     , document.getElementById("root"));
