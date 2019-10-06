2feace4db200470c654b4e9da59d3372bbbfcff9
Ishan Ghorela <ishan.ghorela@naukri.com>
2019-10-06 19:41:43 +0530

    boilerplate changes ...........................

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ class App extends Component {
     render() {
         return(
             <div>
-                <h1>Let's begin with HimSafar !!</h1>
-                {/* <FormattedMessage
-                    id="app.intro"
-                /> */}
+                <h1>Hey, you are ready to create your first react app without CRA</h1>
+                <p>
+                    This project is a boiler plate for the react app. You can start building your 
+                    app by creating custom Components in the src/components folder. 
+                    Use command "npm run start" to run the project.
+                </p>
             </div>
         );
     }
