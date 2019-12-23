7a728c5b0009ca02f8bec85c873da532e7795654
Zaheb Shamsi <Zaheb@Etechaces.com>
2019-12-23 23:33:17 +0530

    First  Commit

--- src/index.jsx
+++ src/index.jsx
@@ -7,22 +7,15 @@ import rootReducer from "./services/rootReducer";
 import saga from "./services/saga";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import routes from "./core/Routing/router";
-// import { IntlProvider } from "react-intl";
-// import translation from "./core/Internationalization/internationalization";
-
 
 const storeMain = store.provideStore(rootReducer, saga);
 
 ReactDOM.render(
-    // <IntlProvider locale={translation.getLanguage()} messages={translation.getTransalations()}>
     <Provider store={storeMain}>
         <Router>
             <Switch>
-                {/* <Route exact path="/" component={App} />
-                <Route exact path="/about" component={About} />  */}
                 { routes.map( route => <Route  {...route}/>) }
             </Switch>
         </Router>
     </Provider>
-    // </IntlProvider>
     , document.getElementById("root"));
